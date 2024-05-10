d0785a35ecf3c00fc91c9723da870c38a08108f2
Griffin Carr <griffin@goodshuffle.com>
2024-05-10 14:00:18 -0400

    section on custom directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgClass, NgIf, NgStyle, NgSwitch, NgSwitchCase, NgSwitchDefault} from "@angular/common";
+import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
+import {BetterHighlightDirective} from "./better-highlight/better-highlight.directive";
+import {UnlessDirective} from "./unless/unless.directive";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgClass, NgStyle, BasicHighlightDirective, BetterHighlightDirective, UnlessDirective, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'directives_deepdive';
+  numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
+  value = 10;
 }
